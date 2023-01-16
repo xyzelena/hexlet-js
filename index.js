205f85007ea5776f6f9139cf7d7aae723b3da07f
xyzelena <emkhuzhina@gmail.com>
2023-01-16 21:39:10 +0530

    save README.md index.js with prettier

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ console.log(_.intersection([2, 1], [2, 3])); // => [2]
 // capitalize делает первую букву заглавной
 console.log(_.capitalize("hello")); // Hello
 
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
+
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
