350b14fb157370e315ec629d8bf8f34107f8df92
xyzelena <emkhuzhina@gmail.com>
2023-01-16 22:27:30 +0530

    fix index.js with eslint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-console.log(Math.round(5.34));
-
 // По общепринятому соглашению, lodash импортируется под именем _
-import _ from "lodash";
+import _ from 'lodash';
+
+console.log(Math.round(5.34));
 
 // Пересечение массивов
 console.log(_.intersection([2, 1], [2, 3])); // => [2]
 
 // capitalize делает первую букву заглавной
-console.log(_.capitalize("hello")); // Hello
+console.log(_.capitalize('hello')); // Hello
 
-console.log(_.last(["one", "two"]));
+console.log(_.last(['one', 'two']));
 
 const fn = (a, b) => {
   const c = a + b;
